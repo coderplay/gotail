7f70871fd30260ca0c1fd4fd117bb1fc658311cf
Min Zhou <coderplay@gmail.com>
2014-06-19 18:04:49 -0700

    Modified gofmt to use 2 spaces instead of a tab for indent

--- tail.go
+++ tail.go
@@ -8,41 +8,40 @@ import "flag"
  * 3. each coroutine do : watch dir event and consume the event, send
  */
 func main() {
-	// parse config file path from command line
-	configFilePtr := flag.String("conf", "config.ini", "Config file name")
-	flag.Parse()
+  // parse config file path from command line
+  configFilePtr := flag.String("conf", "config.ini", "Config file name")
+  flag.Parse()
 
-	// read config file, the config file is ini format
-	ParseFromIni(*configFilePtr)
+  // read config file, the config file is ini format
+  ParseFromIni(*configFilePtr)
 
-
-//
-//	watcher, err := fsnotify.NewWatcher()
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	done := make(chan bool)
-//
-//	// Process events
-//	go func() {
-//		for {
-//			select {
-//			case ev := <-watcher.Event:
-//				log.Println("event:", ev)
-//			case err := <-watcher.Error:
-//				log.Println("error:", err)
-//			}
-//		}
-//	}()
-//
-//	err = watcher.Watch("testDir")
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	<-done
-//
-//	/* ... do stuff ... */
-//	watcher.Close()
+  //
+  //	watcher, err := fsnotify.NewWatcher()
+  //	if err != nil {
+  //		log.Fatal(err)
+  //	}
+  //
+  //	done := make(chan bool)
+  //
+  //	// Process events
+  //	go func() {
+  //		for {
+  //			select {
+  //			case ev := <-watcher.Event:
+  //				log.Println("event:", ev)
+  //			case err := <-watcher.Error:
+  //				log.Println("error:", err)
+  //			}
+  //		}
+  //	}()
+  //
+  //	err = watcher.Watch("testDir")
+  //	if err != nil {
+  //		log.Fatal(err)
+  //	}
+  //
+  //	<-done
+  //
+  //	/* ... do stuff ... */
+  //	watcher.Close()
 }
